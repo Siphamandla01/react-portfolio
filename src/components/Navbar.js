9df6cb76fa6f8ad9386fb5e3fea8e28685337f9e
Siphamandla Mncube <88335645+Siphamandla01@users.noreply.github.com>
2022-11-03 14:47:55 +0200

    created navbar with links

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import SovTechLogo from "../sovtech_logo-2.png";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container">
+				<a className="navbar-brand ps-5" href="#">
+					<img src={SovTechLogo} alt="SovTech Logo" className="logo" />
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div
+					className="collapse navbar-collapse pe-5"
+					id="navbarSupportedContent"
+				>
+					<ul className="navbar-nav ms-auto">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								about
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Education
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
