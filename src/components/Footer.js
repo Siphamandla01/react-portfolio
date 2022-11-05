313c8646a59a07b304b3edff34840fd819a6786e
Siphamandla Mncube <88335645+Siphamandla01@users.noreply.github.com>
2022-11-05 15:10:21 +0200

    added a footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="bg-light text-center text-lg-start">
+			<div
+				className="text-center p-3"
+				style={{
+					backgroundColor: "#fff",
+				}}
+			>
+				© 2022 Copyright: Siphamandla Mncube &#128522;
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
